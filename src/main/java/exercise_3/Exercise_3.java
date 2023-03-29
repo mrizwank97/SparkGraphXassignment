fb950423e2ca4ecb4a860bcaf6e33a705da4ba58
Chidiebere Ogbuchi <Chiboyogbuchi@gmail.com>
2023-03-29 12:01:19 +0200

    Comments added

--- src/main/java/exercise_3/Exercise_3.java
+++ src/main/java/exercise_3/Exercise_3.java
@@ -1,15 +1,3 @@
-//package exercise_3;
-//
-//import org.apache.spark.api.java.JavaSparkContext;
-//
-//public class Exercise_3 {
-//
-//    public static void shortestPathsExt(JavaSparkContext ctx) {
-//
-//    }
-//
-//}
-
 package exercise_3;
 
         import com.google.common.collect.ImmutableMap;
@@ -30,218 +18,58 @@ package exercise_3;
         import java.util.*;
 
 
-// to keep track of the shortest path, we can include an additional stored value in each
-// vertex of the path that we used to get there. Then, whenever we have a new extension
-// to the path, we add in the path that was stored in sourceVertex.
-//
-// Essentially, we will change the parameter VD from Ex2 from an Integer that stored the distance to a
-// list that stores the distance and a list containing the shortest path to get to that vertex
-// [ID, [shortestDistanceValue, [shortestPathToThisID]]]
-// This will require us to update every place the methods are called or defined to ensure we have consistency
-// in the new class being used. Any place where [VD] is referenced is where we need to look.
-// Our return values also need to be update to return our new version of VD instead of just an integer.
-//
-// reference: https://www.geeksforgeeks.org/printing-paths-dijkstras-shortest-path-algorithm/
-// reference: GraphX https://spark.apache.org/docs/latest/graphx-programming-guide.html
-
-
-//public class Exercise_3  {
-//
-//    private static class VProg extends AbstractFunction3<Long,Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>> implements Serializable {
-//        @Override
-//        public Tuple2<Integer,List<Object>> apply(Long vertexID, Tuple2<Integer,List<Object>> vertexValue, Tuple2<Integer,List<Object>> message) {
-//
-////            System.out.println("1. VProg: vertexID: " + vertexID + ". vertexValue: " + vertexValue._1 + ". message: " + message._1);
-//            if (vertexValue._1 <= message._1) {
-//                return vertexValue;
-//            }
-//            else {
-//                return message;
-//            }
-//        }
-//    }
-//
-//    private static class sendMsg extends AbstractFunction1<EdgeTriplet<Tuple2<Integer,List<Object>>,Integer>, Iterator<Tuple2<Object,Tuple2<Integer,List<Object>>>>> implements Serializable {
-//        @Override
-//        // The triplet has values of ((SourceVertexID, (SourceVertexValue, SourceVertexPath)), (DstVertexID, (DstVertexValue, DstVertexPath), (EdgeValue))
-//
-//        public Iterator<Tuple2<Object,Tuple2<Integer,List<Object>>>> apply(EdgeTriplet<Tuple2<Integer,List<Object>>, Integer> triplet) {
-//            // to make things more readable, I define all the variables.
-//            Tuple2<Object,Tuple2<Integer,List<Object>>> sourceVertex = triplet.toTuple()._1();
-//            Integer sourceVertexCurrentDistance = sourceVertex._2._1;
-//            List sourceVertexShortestPath = sourceVertex._2._2;
-//
-//            Tuple2<Object,Tuple2<Integer,List<Object>>> dstVertex = triplet.toTuple()._2();
-//            Integer dstVertexCurrentDistance = dstVertex._2._1;
-//
-//            Integer edgeDistance = triplet.toTuple()._3();
-//
-//            // this if statement required, otherwise the MAX_VALUE is increased by edge distance and becomes negative.
-//            if  (sourceVertexCurrentDistance == Integer.MAX_VALUE) {
-//                return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>().iterator()).asScala();
-//            }
-//
-//            else if (sourceVertexCurrentDistance + edgeDistance <= dstVertexCurrentDistance) {
-//                // here we will append the ID of the dstVertex to the shortest path that existed in the source vertex
-//                // this will be returned and sent in the message
-//                List<Object> shortestPath = sourceVertexShortestPath;
-//                shortestPath.add(Long.parseLong(String.valueOf(dstVertex._1)));
-////                System.out.println("2. sendMsg IF: srcDist (" + sourceVertex._2 + ") + edgeDist (" + edgeDistance + ") <= dstDist (" + dstVertex._2 + ")");
-////                System.out.println("2. sendMsg IF: shortestPath" + Collections.addAll(sourceVertexShortestPath, triplet.dstId()));
-////                System.out.println("2. Shortest Path: " + shortestPath);
-////                System.out.println("");
-//
-//                return JavaConverters.asScalaIteratorConverter(Arrays.asList(new Tuple2<Object,Tuple2<Integer,List<Object>>>(triplet.dstId(), new Tuple2<Integer,List<Object>>(sourceVertexCurrentDistance + edgeDistance, shortestPath))).iterator()).asScala();
-//
-//            } else {
-//                // do nothing
-//                System.out.println("2. sendMsg ELSE: sourceVertex._2 (" + sourceVertex._2 + ") + edgeDistance (" + edgeDistance + ") <= dstVertex._2(" + dstVertex._2 + ")");
-//                System.out.println("2. Return array: " + new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>());
-//                System.out.println("");
-//
-//                return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>().iterator()).asScala();
-//
-//            }
-//        }
-//    }
-//
-//    private static class merge extends AbstractFunction2<Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>> implements Serializable {
-//        @Override
-//
-//        public Tuple2<Integer, List<Object>> apply(Tuple2<Integer, List<Object>> o, Tuple2<Integer, List<Object>> o2) {
-//            // instead of using the method from exercise to, we use an if statement so that we can return the full
-//            // VD parameter while only comparing the distances.
-////            System.out.println("3. merge: o" + o + ". o2: " + o2 + ". return: " + Math.min(o._1, o2._1));
-////            System.out.println("");
-////            System.out.println("MERGE!");
-//            if (o._1 <= o2._1) {
-//                return o;
-//            }
-//            else {
-//                return o2;
-//            }
-//        }
-//    }
-//
-//    public static void shortestPathsExt(JavaSparkContext ctx) {
-//        Map<Long, String> labels = ImmutableMap.<Long, String>builder()
-//                .put(1l, "A")
-//                .put(2l, "B")
-//                .put(3l, "C")
-//                .put(4l, "D")
-//                .put(5l, "E")
-//                .put(6l, "F")
-//                .build();
-//
-//
-//        // here is where the magic happens. We replaced the Integer with a Tuple2 that contains the shortestPath distance
-//        // as well as a list that will be appended to in order to keep track of the path.
-//        List<Tuple2<Object,Tuple2<Integer,List<Object>>>> vertices = Lists.newArrayList(
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(1l, new Tuple2<Integer,List<Object>>(0, Lists.newArrayList(1l))),
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(2l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList())),
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(3l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList())),
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(4l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList())),
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(5l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList())),
-//                new Tuple2<Object,Tuple2<Integer,List<Object>>>(6l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList()))
-//        );
-//
-//
-//        List<Edge<Integer>> edges = Lists.newArrayList(
-//                new Edge<Integer>(1l,2l, 4), // A --> B (4)
-//                new Edge<Integer>(1l,3l, 2), // A --> C (2)
-//                new Edge<Integer>(2l,3l, 5), // B --> C (5)
-//                new Edge<Integer>(2l,4l, 10), // B --> D (10)
-//                new Edge<Integer>(3l,5l, 3), // C --> E (3)
-//                new Edge<Integer>(5l, 4l, 4), // E --> D (4)
-//                new Edge<Integer>(4l, 6l, 11) // D --> F (11)
-//        );
-//
-//
-//        // Through all of the following code, we have to update our class to match the new VD
-//        JavaRDD<Tuple2<Object,Tuple2<Integer,List<Object>>>> verticesRDD = ctx.parallelize(vertices);
-//        JavaRDD<Edge<Integer>> edgesRDD = ctx.parallelize(edges);
-//
-//        // note: here we updated the VD classtage from Integer to Tuple2
-//        // TODO: What exactly is this classtag, and why wouldn't I include a full Tuple2<int<List>> type of definition?
-//        Graph<Tuple2<Integer,List<Object>>,Integer> G = Graph.apply(verticesRDD.rdd(),edgesRDD.rdd(),new Tuple2<Integer,List<Object>>(0, Lists.newArrayList(1l)), StorageLevel.MEMORY_ONLY(), StorageLevel.MEMORY_ONLY(),
-//                scala.reflect.ClassTag$.MODULE$.apply(Tuple2.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
-//
-//        GraphOps ops = new GraphOps(G, scala.reflect.ClassTag$.MODULE$.apply(Tuple2.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
-//
-//        // Here we have to update the pregel parameter "A" as it is the type for the initial message that is passed to
-//        // all the vertexes
-//        ops.pregel(new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList()), // initialMsg
-//                        Integer.MAX_VALUE,      // maxIterations
-//                        EdgeDirection.Out(),    //activeDirection
-//                        new VProg(),
-//                        new sendMsg(),
-//                        new merge(),
-//                        ClassTag$.MODULE$.apply(Tuple2.class))      // VD classTag
-//                .vertices()
-//                .toJavaRDD()
-//                .foreach(v -> {
-//                    // TODO: Need to add a way to pull the list vertex._2._1 and remove all the duplicates before printing.
-//                    Tuple2<Object,Tuple2<Integer,List<Object>>> vertex = (Tuple2<Object,Tuple2<Integer,List<Object>>>)v;
-//                    System.out.println("Minimum cost to get from "+labels.get(1l)+" to "+labels.get(vertex._1)+" is "+vertex._2._2+" with cost "+vertex._2._1);
-//                });
-//    }
-//
-//}
-
-
 public class Exercise_3  {
 
+    // Vertex Program implementation
     private static class VProg extends AbstractFunction3<Long,Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>,Tuple2<Integer,List<Object>>> implements Serializable {
         @Override
         public Tuple2<Integer,List<Object>> apply(Long vertexID, Tuple2<Integer,List<Object>> vertexValue, Tuple2<Integer,List<Object>> message) {
 
-//            System.out.println("1. VProg: vertexID: " + vertexID + ". vertexValue: " + vertexValue._1 + ". message: " + message._1);
-            if (vertexValue._1 <= message._1) {
-                return vertexValue;
+            if (vertexValue._1 <= message._1) {  // if the current vertex value is less than or equal to the incoming message value
+                return vertexValue; // keep the current vertex value
             }
             else {
-                return message;
+                return message; // else update the vertex value with the incoming message value
             }
         }
     }
+
+
 
     private static class sendMsg extends AbstractFunction1<EdgeTriplet<Tuple2<Integer,List<Object>>,Integer>, Iterator<Tuple2<Object,Tuple2<Integer,List<Object>>>>> implements Serializable {
         @Override
         // The triplet has values of ((SourceVertexID, (SourceVertexValue, SourceVertexPath)), (DstVertexID, (DstVertexValue, DstVertexPath), (EdgeValue))
 
         public Iterator<Tuple2<Object,Tuple2<Integer,List<Object>>>> apply(EdgeTriplet<Tuple2<Integer,List<Object>>, Integer> triplet) {
-            // to make things more readable, I define all the variables.
-            Tuple2<Object,Tuple2<Integer,List<Object>>> sourceVertex = triplet.toTuple()._1();
-            Integer sourceVertexCurrentDistance = sourceVertex._2._1;
-            List sourceVertexShortestPath = sourceVertex._2._2;
 
-            Tuple2<Object,Tuple2<Integer,List<Object>>> dstVertex = triplet.toTuple()._2();
-            Integer dstVertexCurrentDistance = dstVertex._2._1;
+            Tuple2<Object,Tuple2<Integer,List<Object>>> sourceVertex = triplet.toTuple()._1(); // the source vertex
+            Integer sourceVertexCurrentDistance = sourceVertex._2._1; // the current distance of the source vertex
+            List sourceVertexShortestPath = sourceVertex._2._2; // the current shortest path to the source vertex
 
-            Integer edgeDistance = triplet.toTuple()._3();
+            Tuple2<Object,Tuple2<Integer,List<Object>>> dstVertex = triplet.toTuple()._2(); // the destination vertex
+            Integer dstVertexCurrentDistance = dstVertex._2._1; // the current distance of the destination vertex
+
+            Integer edgeDistance = triplet.toTuple()._3(); // the distance of the edge
 
             // this if statement required, otherwise the MAX_VALUE is increased by edge distance and becomes negative.
             if  (sourceVertexCurrentDistance == Integer.MAX_VALUE) {
                 return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>().iterator()).asScala();
+                // return an empty iterator if the current source vertex distance is Integer.MAX_VALUE
             }
 
             else if (sourceVertexCurrentDistance + edgeDistance <= dstVertexCurrentDistance) {
-                // here we will append the ID of the dstVertex to the shortest path that existed in the source vertex
-                // this will be returned and sent in the message
+                // if the sum of the source vertex distance and the edge distance is less than or equal to the current distance of the destination vertex
+
                 List<Object> shortestPath = sourceVertexShortestPath;
                 shortestPath.add(Long.parseLong(String.valueOf(dstVertex._1)));
-//                System.out.println("2. sendMsg IF: srcDist (" + sourceVertex._2 + ") + edgeDist (" + edgeDistance + ") <= dstDist (" + dstVertex._2 + ")");
-//                System.out.println("2. sendMsg IF: shortestPath" + Collections.addAll(sourceVertexShortestPath, triplet.dstId()));
-//                System.out.println("2. Shortest Path: " + shortestPath);
-//                System.out.println("");
 
                 return JavaConverters.asScalaIteratorConverter(Arrays.asList(new Tuple2<Object,Tuple2<Integer,List<Object>>>(triplet.dstId(), new Tuple2<Integer,List<Object>>(sourceVertexCurrentDistance + edgeDistance, shortestPath))).iterator()).asScala();
 
             } else {
                 // do nothing
                 System.out.println("2. sendMsg ELSE: sourceVertex._2 (" + sourceVertex._2 + ") + edgeDistance (" + edgeDistance + ") <= dstVertex._2(" + dstVertex._2 + ")");
-                System.out.println("2. Return array: " + new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>());
-                System.out.println("");
+                System.out.println("2. Return array: " + new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>()); // copy the current shortest path of the source vertex
+                System.out.println(""); // add the destination vertex
 
                 return JavaConverters.asScalaIteratorConverter(new ArrayList<Tuple2<Object,Tuple2<Integer,List<Object>>>>().iterator()).asScala();
 
@@ -253,14 +81,13 @@ public class Exercise_3  {
         @Override
 
         public Tuple2<Integer, List<Object>> apply(Tuple2<Integer, List<Object>> o, Tuple2<Integer, List<Object>> o2) {
-            // instead of using the method from exercise to, we use an if statement so that we can return the full
-            // VD parameter while only comparing the distances.
-//            System.out.println("3. merge: o" + o + ". o2: " + o2 + ". return: " + Math.min(o._1, o2._1));
-//            System.out.println("");
-//            System.out.println("MERGE!");
+
+            // If the key of the first tuple is less than or equal to the key of the second tuple,
+            // return the first tuple
             if (o._1 <= o2._1) {
                 return o;
             }
+            // Otherwise, return the second tuple
             else {
                 return o2;
             }
@@ -278,8 +105,6 @@ public class Exercise_3  {
                 .build();
 
 
-        // here is where the magic happens. We replaced the Integer with a Tuple2 that contains the shortestPath distance
-        // as well as a list that will be appended to in order to keep track of the path.
         List<Tuple2<Object,Tuple2<Integer,List<Object>>>> vertices = Lists.newArrayList(
                 new Tuple2<Object,Tuple2<Integer,List<Object>>>(1l, new Tuple2<Integer,List<Object>>(0, Lists.newArrayList(1l))),
                 new Tuple2<Object,Tuple2<Integer,List<Object>>>(2l, new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList())),
@@ -305,15 +130,13 @@ public class Exercise_3  {
         JavaRDD<Tuple2<Object,Tuple2<Integer,List<Object>>>> verticesRDD = ctx.parallelize(vertices);
         JavaRDD<Edge<Integer>> edgesRDD = ctx.parallelize(edges);
 
-        // note: here we updated the VD classtage from Integer to Tuple2
-        // TODO: What exactly is this classtag, and why wouldn't I include a full Tuple2<int<List>> type of definition?
+
         Graph<Tuple2<Integer,List<Object>>,Integer> G = Graph.apply(verticesRDD.rdd(),edgesRDD.rdd(),new Tuple2<Integer,List<Object>>(0, Lists.newArrayList(1l)), StorageLevel.MEMORY_ONLY(), StorageLevel.MEMORY_ONLY(),
                 scala.reflect.ClassTag$.MODULE$.apply(Tuple2.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
 
         GraphOps ops = new GraphOps(G, scala.reflect.ClassTag$.MODULE$.apply(Tuple2.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
 
-        // Here we have to update the pregel parameter "A" as it is the type for the initial message that is passed to
-        // all the vertexes
+
         ops.pregel(new Tuple2<Integer,List<Object>>(Integer.MAX_VALUE, Lists.newArrayList()), // initialMsg
                         Integer.MAX_VALUE,      // maxIterations
                         EdgeDirection.Out(),    //activeDirection
@@ -324,7 +147,7 @@ public class Exercise_3  {
                 .vertices()
                 .toJavaRDD()
                 .foreach(v -> {
-                    // TODO: Need to add a way to pull the list vertex._2._1 and remove all the duplicates before printing.
+
                     Tuple2<Object,Tuple2<Integer,List<Object>>> vertex = (Tuple2<Object,Tuple2<Integer,List<Object>>>)v;
                     System.out.println("Minimum cost to get from "+labels.get(1l)+" to "+labels.get(vertex._1)+" is "+vertex._2._2+" with cost "+vertex._2._1);
                 });
--- src/main/java/exercise_4/Exercise_4_warmup.java
+++ src/main/java/exercise_4/Exercise_4_warmup.java
@@ -13,7 +13,7 @@ import org.graphframes.GraphFrame;
 
 public class Exercise_4_warmup {
 
-	
+
 	public static void warmup(JavaSparkContext ctx, SQLContext sqlCtx) {
 		java.util.List<Row> vertices_list = new ArrayList<Row>();
 		vertices_list.add(RowFactory.create("a", "Alice", 34));
@@ -22,24 +22,24 @@ public class Exercise_4_warmup {
 		vertices_list.add(RowFactory.create("d", "David", 29));
 		vertices_list.add(RowFactory.create("e", "Esther", 32));
 		vertices_list.add(RowFactory.create("f", "Fanny", 36));
-		vertices_list.add(RowFactory.create("g", "Gabby", 60));		
-		
+		vertices_list.add(RowFactory.create("g", "Gabby", 60));
+
 		JavaRDD<Row> vertices_rdd = ctx.parallelize(vertices_list);
-		
-		
+
+
 		StructType vertices_schema = new StructType(new StructField[]{
-			new StructField("id", DataTypes.StringType, true, new MetadataBuilder().build()),
-			new StructField("name", DataTypes.StringType, true, new MetadataBuilder().build()),
-			new StructField("age", DataTypes.IntegerType, true, new MetadataBuilder().build())
+				new StructField("id", DataTypes.StringType, true, new MetadataBuilder().build()),
+				new StructField("name", DataTypes.StringType, true, new MetadataBuilder().build()),
+				new StructField("age", DataTypes.IntegerType, true, new MetadataBuilder().build())
 		});
-		
+
 		Dataset<Row> vertices =  sqlCtx.createDataFrame(vertices_rdd, vertices_schema);
-		
-		
-		
+
+
+
 		// edges creation
 		java.util.List<Row> edges_list = new ArrayList<Row>();
-		
+
 		edges_list.add(RowFactory.create("a", "b", "friend"));
 		edges_list.add(RowFactory.create("b", "c", "follow"));
 		edges_list.add(RowFactory.create("c", "b", "follow"));
@@ -48,24 +48,24 @@ public class Exercise_4_warmup {
 		edges_list.add(RowFactory.create("e", "d", "friend"));
 		edges_list.add(RowFactory.create("d", "a", "friend"));
 		edges_list.add(RowFactory.create("a", "e", "friend"));
-		
+
 		JavaRDD<Row> edges_rdd = ctx.parallelize(edges_list);
-			
-		
+
+
 		StructType edges_schema = new StructType(new StructField[]{
-			new StructField("src", DataTypes.StringType, true, new MetadataBuilder().build()),
-			new StructField("dst", DataTypes.StringType, true, new MetadataBuilder().build()),
-			new StructField("relationship", DataTypes.StringType, true, new MetadataBuilder().build())
+				new StructField("src", DataTypes.StringType, true, new MetadataBuilder().build()),
+				new StructField("dst", DataTypes.StringType, true, new MetadataBuilder().build()),
+				new StructField("relationship", DataTypes.StringType, true, new MetadataBuilder().build())
 		});
 
 		Dataset<Row> edges = sqlCtx.createDataFrame(edges_rdd, edges_schema);
-		
+
 		GraphFrame gf = GraphFrame.apply(vertices,edges);
 
 		System.out.println(gf);
-	
+
 		gf.edges().show();
 		gf.vertices().show();
 	}
-	
+
 }
